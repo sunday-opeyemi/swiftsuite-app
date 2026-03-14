8591c90f0a72a86e8e1d08aa42335525da736aec
Yemi <sunday.opeyemi2018@gmail.com>
2026-03-14 23:05:00 +0100

    A new update were made

--- app/inventoryApp/serializer.py
+++ app/inventoryApp/serializer.py
@@ -6,7 +6,7 @@ class InventoryModelUpdateSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = InventoryModel
-		exclude = ('id', 'user', 'last_updated', 'item_specific_fields', 'map_status', 'date_created', 'active', 'vendor_name', 'market_name', 'ends_status', 'market_item_url', 'vendor_identifier', 'manual_map')
+		exclude = ('id', 'user', 'last_updated', 'map_status', 'date_created', 'active', 'vendor_name', 'market_name', 'ends_status', 'market_item_url', 'vendor_identifier', 'manual_map')
 
 	def update(self, instance, validated_data):
 		validated_data.pop('user_id', None)
