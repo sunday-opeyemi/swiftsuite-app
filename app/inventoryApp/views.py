0c2cb36a35ac51e847ea8de0cba78d0f13c948b1
Yemi <sunday.opeyemi2018@gmail.com>
2026-03-14 21:41:55 +0100

    A new update were made

--- app/inventoryApp/views.py
+++ app/inventoryApp/views.py
@@ -790,7 +790,7 @@ class MarketInventory:
             else:
                 return Response(f"Invalid data: {serializer.errors}", status=status.HTTP_400_BAD_REQUEST)
             # convert item specific field into xml
-            # xml_item_specifics = minv.json_to_xml(json.loads(product_info.item_specific_fields))
+            xml_item_specifics = minv.json_to_xml(product_info.item_specific_fields)
             # Get the calculated minimum offer price of product going to ebay
             try:
                 minimum_offer_price = eb.calculated_minimum_offer_price(validated_data['start_price'], validated_data['min_profit_mergin'], validated_data['profit_margin'])
