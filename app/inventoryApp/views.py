5a580ba2757466c789be57679f5efba649dd0866
Yemi <sunday.opeyemi2018@gmail.com>
2026-03-25 13:06:42 +0100

    A new update were made

--- app/inventoryApp/views.py
+++ app/inventoryApp/views.py
@@ -410,7 +410,7 @@ def update_product_on_marketplace(request, userid, market_name, inventory_id):
             if response == "Success":
                 return Response(f"Product updated successfully", status=status.HTTP_200_OK)
             else:
-                return Response(f"Error updating product on Ebay", status=status.HTTP_400_BAD_REQUEST)
+                return Response(f"Error updating product on Ebay: {response}", status=status.HTTP_400_BAD_REQUEST)
 
         elif market_name == "Woocommerce":
             response = wooc.update_woocommerce_product(request, userid, market_name, inventory_id)
@@ -626,10 +626,10 @@ class MarketInventory:
                 serializer.save()
                 return "Success"
             else:
-                return "Error"
+                return response.text
 
         except Exception as e:
-            return "Error"
+            return e
      
     
     # Get all product already listed on Ebay from the inventory
